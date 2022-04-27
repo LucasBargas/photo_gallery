bbc9d22772b7eb79549b004d4f614e65c0cb4143
lucas.bargasoficial@gmail.com <lucas.bargasoficial@gmail.com>
2022-04-26 21:08:52 -0300

    Funcionalidade de pesquisa e filtro de fotos - desenvolvida

--- src/pages/HomePage/PhotosSection/PhotosSection.js
+++ src/pages/HomePage/PhotosSection/PhotosSection.js
@@ -1,33 +1,43 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import * as S from './PhotosSection.styles';
 import Container from '../../../styles/Container';
 import photos from './photos';
-
-console.log(photos)
 
 const PhotosSection = ({ searchValue, checked }) => {
   const photosFilter = photos.filter(({ category, defaultCategory }) => {
     return checked.length ? category.toLocaleLowerCase().includes(searchValue.toLocaleLowerCase()) && (checked.indexOf(category) !== -1 || checked.indexOf(defaultCategory) !== -1) : searchValue.length && category.toLocaleLowerCase().includes(searchValue.toLocaleLowerCase()); 
   })
 
-  return (
-    <S.PhotosSectionContainer>
-      <Container>
-        <S.PhotosArea checked={checked} searchValue={searchValue}>
-          {photosFilter.map(({ photo, id }) => (
-            <S.PhotoCard key={`photo ${id}`}>
-              <img src={photo} alt="" />
-            </S.PhotoCard>
-          ))}
-          {(checked.length === 0 && searchValue.length === 0) && (
-            <S.ErrorFilter>
-              <p>Você precisa filtrar a categoria de fotos desejada ou pesquisar por uma.</p>
-            </S.ErrorFilter>
-          )}
-        </S.PhotosArea>
-      </Container>
-    </S.PhotosSectionContainer>
-  )
+  useEffect(() => {
+    console.log(searchValue)
+  }, [searchValue]);
+
+  if (searchValue.length || checked.length > 0 ) {
+    return (
+      <S.PhotosSectionContainer>
+        <Container>
+          <S.PhotosArea>
+            {photosFilter.map(({ photo, id }) => (
+              <S.PhotoCard key={`photo ${id}`}>
+                <img src={photo} alt="" />
+              </S.PhotoCard>
+            ))}
+          </S.PhotosArea>
+        </Container>
+      </S.PhotosSectionContainer>
+      
+    )
+  } else {
+    return (
+      <S.ErrorFilter>
+        <Container>
+          <p>Você precisa filtrar a categoria de fotos desejada ou pesquisar por uma.</p>
+        </Container>
+      </S.ErrorFilter>
+    )
+  }
+
+  
 }
 
 export default PhotosSection;
--- src/pages/HomePage/PhotosSection/PhotosSection.styles.js
+++ src/pages/HomePage/PhotosSection/PhotosSection.styles.js
@@ -1,26 +1,31 @@
 import styled from 'styled-components';
-import { css } from 'styled-components';
 
 export const PhotosSectionContainer = styled.section`
   padding: 1.5rem 0;
 `;
 
 export const PhotosArea = styled.div`
-  ${({ checked, searchValue }) => (checked.length > 0 || searchValue.length) && css`
-    display: grid;
-    grid-template-columns: repeat(4, 1fr);
-    gap: .75rem;
-  `}
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  gap: .75rem;
   
 `;
 
 export const PhotoCard = styled.figure`
+  overflow: hidden;
+
   img {
     display: block;
     width: 100%;
+    cursor: pointer;
+    transition: .4s ease;
+
+    &:hover {
+      transform: scale(1.2);
+    }
   }
 `;
 
 export const ErrorFilter = styled.div`
-
+  padding-top: .75rem;
 `;
